d85602992b7bc830e7b28699c7efc3076098b8bf
Dávid Vörös <itkutya@student.elte.hu>
2024-03-08 19:12:10 +0100

    Add project files.

---
+++ InariKonKon/src/Application.cpp
@@ -0,0 +1,8 @@
+#include "Application.hpp"
+
+#include <print>
+
+void Application::test()
+{
+	std::print("Hello libs!\n");
+}
---
+++ TianyaGame/main.cpp
@@ -0,0 +1,8 @@
+#include "Application.hpp"
+
+int main()
+{
+	Application app;
+	app.test();
+	return 0;
+}
