2faf1f8ac047ede2b6b559a57631e3ad6c35ea3c
Dávid Vörös <itkutya@student.elte.hu>
2024-03-16 22:00:01 +0100

    Added a singleton util class.

--- InariKonKon/include/InariKonKon/Utility/Singleton.hpp
+++ InariKonKon/include/InariKonKon/Utility/Singleton.hpp
@@ -1,9 +1,32 @@
 #pragma once
 
+#include "NonCopyable.hpp"
+
 namespace ikk
 {
-	class Singleton
+	template<class T>
+	class Singleton : public NonCopyable
 	{
+    public:
+        constexpr Singleton(const Singleton&) noexcept = delete;
+        constexpr Singleton(Singleton&) noexcept = delete;
+        constexpr Singleton(Singleton&&) noexcept = delete;
 
+        virtual constexpr Singleton& operator=(const Singleton&) noexcept = delete;
+        virtual constexpr Singleton& operator=(Singleton&) noexcept = delete;
+        virtual constexpr Singleton& operator=(Singleton&&) noexcept = delete;
+        
+        virtual constexpr ~Singleton() noexcept = default;
+
+        [[nodiscard]] static T& getInstance() noexcept;
+    protected:
+        constexpr Singleton() noexcept = default;
 	};
+
+    template<class T>
+    inline T& Singleton<T>::getInstance() noexcept
+    {
+        static T instance;
+        return instance;
+    }
 }
