d0820253b3da6f12c3e255d0fb4c60d3bb860451
Dávid Vörös <itkutya@student.elte.hu>
2024-06-10 19:38:22 +0200

    Removed unnecessary files... Will be added back when engine is ready.

--- Tianya/main.cpp
+++ Tianya/main.cpp
@@ -1,4 +1,41 @@
-#include "TestScene.h"
+#include "InariKonKon.hpp"
+
+#include "InariKonKon/Graphics/Shapes/Triangle.hpp"
+#include "InariKonKon/Graphics/Shapes/Quad.hpp"
+
+class TestScene final : public ikk::Scene
+{
+public:
+	TestScene(ikk::Application& app) noexcept : ikk::Scene(app, ikk::PostEffects::None) {};
+
+	TestScene(const TestScene&) noexcept = default;
+	TestScene(TestScene&&) noexcept = default;
+
+	virtual TestScene& operator=(const TestScene&) noexcept = default;
+	virtual TestScene& operator=(TestScene&&) noexcept = default;
+
+	~TestScene() noexcept = default;
+
+	void handleEvents(const ikk::Event& event) noexcept override
+	{
+	};
+
+	void update(const ikk::Time& dt) noexcept override
+	{
+	};
+
+	void render(const ikk::Window& window) const noexcept override
+	{
+		ikk::RenderState state{ &ikk::Shader::getDefaultShaderProgram(), &texture, &camera };
+		window.draw(quad, state);
+		window.draw(triangle, state);
+	};
+private:
+	ikk::Triangle3D triangle;
+	ikk::Quad3D quad;
+	ikk::Texture texture{ std::filesystem::path("wall.jpg") };
+	ikk::Camera<ikk::Projection::Orhto> camera{};
+};
 
 int main()
 {
